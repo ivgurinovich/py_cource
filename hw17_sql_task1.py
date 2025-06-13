e9070cc07073534695273af5f9c8998caae08f37
Igor Gurinovich <ivgurinovich2@gmail.com>
2025-06-13 11:37:27 -0400

    updated hw17_sql_task1.py

--- hw17_sql_task1.py
+++ hw17_sql_task1.py
@@ -26,42 +26,27 @@ def select_all(table):
         conn.close()
 
 
-get_connection()
-
-
-def create_author():
+def create_author(first_name, last_name):
     conn = get_connection()
-    authors = [
-        ('George', 'Orwell'),
-        ('Jane', 'Austen'),
-        ('Mark', 'Twain'),
-        ('Ivan', 'Ivanov')
-    ]
     try:
         with conn:
             with conn.cursor() as cur:
-                cur.executemany(
+                cur.execute(
                     'insert into authors (first_name,last_name) values (%s, %s)',
-                    authors
+                    (first_name, last_name)
                 )
                 print(f"Authors are added:\n")
-                for first_name, last_name in authors:
-                    print(f"- {first_name} {last_name}")
+                print(f"{first_name} {last_name}")
+
+
     except Exception as e:
         print("An error occurred:", e)
     finally:
         conn.close()
 
 
-def add_books():
+def add_books(books):
     conn = get_connection()
-    books = [
-        ('1984', 1, 1949),
-        ('Animal Farm', 1, 1945),
-        ('Pride and Prejudice', 2, 1813),
-        ('Adventures of Huckleberry Finn', 3, 1884),
-        ('Unnamed Book', None, 2001)
-    ]
     try:
         with conn:
             with conn.cursor() as cur:
@@ -69,43 +54,51 @@ def add_books():
                     'insert into books (title, author_id, publication_year) values (%s, %s, %s)',
                     books
                 )
-                print(f"Books are added:\n")
-                for title, author_id, publication_year in books:
-                    print(f"- {title} ")
-
+                print("Books are added:\n")
+                for title, author_id, year in books:
+                    print(f"{title}, {author_id}, {year}")
     except Exception as e:
         print("An error occurred:", e)
     finally:
         conn.close()
 
 
-def add_sales():
+def add_sales(sales):
     conn = get_connection()
-    sales = [
-        (1, 3),
-        (2, 1),
-        (3, 2),
-        (4, 5)
-    ]
     try:
         with conn:
             with conn.cursor() as cur:
                 cur.executemany(
-                    'INSERT INTO sales (book_id, quantity) VALUES (%s, %s)',
+                    'insert into sales (book_id, quantity) values (%s, %s)',
                     sales
                 )
-                print("Sales records added:\n")
-                for book_id, quantity in sales:
-                    print(f"- Book ID: {book_id}, Quantity: {quantity}")
+                print("Sales records added:")
+                for book_id, qty in sales:
+                    print(f"Book ID {book_id}, Quantity {qty}")
     except Exception as e:
         print("An error occurred:", e)
     finally:
         conn.close()
 
 
-create_author()
-add_books()
-add_sales()
+get_connection()
+create_author('George', 'Orwell')
+create_author('Jane', 'Austen')
+create_author('Mark', 'Twain')
+create_author('Ivan', 'Ivanov')
+add_books([
+    ('1984', 1, 1949),
+    ('Animal Farm', 1, 1945),
+    ('Pride and Prejudice', 2, 1813),
+    ('Adventures of Huckleberry Finn', 3, 1884),
+    ('Unnamed Book', None, 2001)
+])
+add_sales([
+    (1, 3),
+    (2, 1),
+    (3, 2),
+    (4, 5)
+])
 select_all('authors')
 select_all('books')
 select_all('sales')
